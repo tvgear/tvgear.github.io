9bda37666a39e84f9601b01bebe717157b8b7fcf
tvgear <79452936+tvgear@users.noreply.github.com>
2022-12-20 20:07:52 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,67 +1,55 @@
 $(document).ready(function () {
-    // CUSTOM SCROLL
-    const lscroll = new LocomotiveScroll({
-        el: document.querySelector('[data-scroll-container]'),
-        smooth: true,
-        lerp: 0.07, 
-        multiplier: 1.5, 
-        reloadOnContextChange: true,
-        touchMultiplier: 2,
-        smoothMobile: 0,
-        smartphone: {
-            smooth: !0,
-            breakpoint: 767
-        },
-        tablet: {
-            smooth: !1,
-            breakpoint: 1024
-        },
+    // OPEN-CLOSED MENU
+    let menuFixed = false;
+    $('.blockMenuFixed__wrap--btnMenu').click(function () {
+        menuFixed = !menuFixed;
+      if (menuFixed === true) {
+        $('.blockMenuFixed').addClass('active');
+        $('.blockMenuFixed__wrap--btnMenu').addClass('active');
+      }
+      else {
+        $('.blockMenuFixed').removeClass('active');
+        $('.blockMenuFixed__wrap--btnMenu').removeClass('active');
+      }
     });
-    // SETUP LOAD LOCOMOTIVE
-    setTimeout(() => {  
-        locoScroll.destroy();
-    }, 0);
-    setTimeout(() => {  
-        locoScroll.init();
-    }, 50);
-    setTimeout(() => {  
-        locoScroll.update();
-    }, 1000);
-    // WRAP HEADER
-    setTimeout(function () {
-        addStyleCSS('.headerWeb', 'hdFixed');
-    }, 2500);
-     // WRAP HEADER 2
-     setTimeout(function () {
-        addStyleCSS('.headerWeb', 'hdLeft');
-    }, 3500);
-    // SCROLL TO DIV
-    var btnMenu = false;
-    $('.itemTab').click(function() {
-        const sectionID = $(this).data('scroll-section-id');
-        lscroll.scrollTo(sectionID);
-        btnMenu = false;
-    });
-    // SCROLL REFRESH
-    function lscrollrefresh(func) {
-        $(window).trigger('resize');
-        func.update();
+    // SCROLL TO ID
+    let anchorlinks = document.querySelectorAll('a[href^="#"]')
+    for (let item of anchorlinks) { // relitere 
+      item.addEventListener('click', (e) => {
+        menuFixed = !menuFixed;
+        $('.blockMenuFixed').removeClass('active');
+        $('.blockMenuFixed__wrap--btnMenu').removeClass('active');
+        let hashval = item.getAttribute('href');
+        let target = document.querySelector(hashval);
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+        history.pushState(null, null, hashval);
+        e.preventDefault();
+      })
     }
-    setTimeout(lscrollrefresh, 200, lscroll);
-    // SHOW DETAIL
-    $(".listProduct__product .buttonShowDetail").click(function() {
-        $(".listProduct__product").removeClass("showDetail");
-        $(this).parent().addClass("showDetail");    
+    // ENTER PRESS SEARCH INVOICE
+    $("#invoiceCode").keydown(function(event) {
+      if (event.keyCode == 13) {
+        checkInvoice();
+      }
     });
-    $(".listProduct__product .buttonHideDetail").click(function() {
-        $(this).parent().removeClass("showDetail");
-    });
-     // SELECT OPTION
-     $(".listProduct__product .blockSelect").children().click(function() {
-        $(this).parent().find(".select").removeClass("select");
-        $(this).addClass("select");
-    });
+    copyBanking();
 });
-function addStyleCSS(elm, classAdd) {
-    $(elm).addClass(classAdd);
+// SEARCH INVOICE
+function checkInvoice() {
+  window.open("https://tvgear.github.io/invoice/" + invoiceCode.value)
+}
+ // COPY BANKING
+function copyBanking() {
+  var copyBtn = $('.wrapInfo__copy');
+  copyBtn.on('click', function() {
+    var content = $(this).prev('.wrapInfo__numberBank');
+    var clipboard = document.createRange();
+    clipboard.selectNode(content[0]);
+    window.getSelection().addRange(clipboard);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+  });
 }
