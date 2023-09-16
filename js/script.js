563da939bf3f431cf416c7fa036d4fccde4ffd7b
votienthuan97 <votienthuan97@gmail.com>
2023-09-16 21:05:10 +0700

    update product

--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@ $(document).ready(function () {
   let loadWeb = $(".blockLoadWeb");
   let loadContent = $(".blockContent");
   let loadBody = $("body");
+  let loadHTML = $("html");
   let itemTab = $(".item__listChild--item");
   let itemCate = $(".blockMenu__listCate--item");
   loadWeb.addClass("active");
   loadBody.addClass("disabled");
+  loadHTML.addClass("disabled");
 
   setTimeout(function () {
     loadWeb.removeClass("active");
     loadContent.removeClass("disabled");
     loadBody.removeClass("disabled");
+    loadHTML.removeClass("disabled");
   }, 5000);
 
   loadBody.click(function () {
