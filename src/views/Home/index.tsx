c7d644d029b87960e86f06ea38259250866ca93d
votienthuan97 <votienthuan97@gmail.com>
2024-06-08 20:27:17 +0700

    add noti

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { BlockHome } from "@/views/Home/style";
-import Logo from "@/components/Logo";
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
   return (
     <BlockHome>
-      {/* <Logo /> */}
       TVGear đang nâng cấp giao diện. Vui lòng quay lại sau
     </BlockHome>
   );
