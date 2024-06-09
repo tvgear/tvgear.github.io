c02d8ef91b5c5cfec4d565477d9c642e60b2f2a6
votienthuan97 <votienthuan97@gmail.com>
2024-06-09 22:46:08 +0700

    remove percent

--- src/layouts/Loading/index.tsx
+++ src/layouts/Loading/index.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { BlockLoading, PercentLoading } from "./style";
+import { BlockLoading } from "./style";
 import Logo from "@/components/Logo";
 
 interface LoadingProps {
@@ -7,32 +6,9 @@ interface LoadingProps {
 }
 
 const Loading: React.FC<LoadingProps> = ({ showLoading }) => {
-  const [percent, setPercent] = useState(0);
-  useEffect(() => {
-    if (showLoading) {
-      let start = Date.now();
-      const duration = 3000;
-      const end = start + duration;
-      const step = () => {
-        let now = Date.now();
-        let progress = Math.min((now - start) / duration, 1);
-        setPercent(Math.floor(progress * 100));
-        if (now < end) {
-          requestAnimationFrame(step);
-        }
-      };
-      requestAnimationFrame(step);
-    } else {
-      setPercent(0);
-    }
-  }, [showLoading]);
-
   return (
     <BlockLoading className={`${showLoading ? "active" : ""}`}>
       <Logo scale="4" moveAni />
-      {percent !== 100 && percent !== 0 && (
-        <PercentLoading>{percent}</PercentLoading>
-      )}
     </BlockLoading>
   );
 };
