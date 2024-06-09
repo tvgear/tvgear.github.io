a36656395ca8f3a486e0882fc827b9a3d70a8516
votienthuan97 <votienthuan97@gmail.com>
2024-06-09 18:12:08 +0700

    add loading page

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { BlockHome, ContentWrap } from "@/views/Home/style";
+import { BlockHome, ContentWrap, TextNote } from "@/views/Home/style";
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
   return (
     <BlockHome>
-      <ContentWrap className="blockContainer"></ContentWrap>
+      <ContentWrap className="blockContainer">
+        <TextNote>Website TVGEAR đang nâng cấp. Vui lòng quay lại sau!</TextNote>
+      </ContentWrap>
     </BlockHome>
   );
 };
