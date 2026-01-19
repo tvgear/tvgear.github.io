5794cf510250997ebcbed1536c99f7bf1ddf61b8
votienthuan97 <votienthuan97@gmail.com>
2026-01-19 19:47:26 +0700

    Update Product

--- src/views/Keyboard/index.tsx
+++ src/views/Keyboard/index.tsx
@@ -3,12 +3,13 @@ import { BlockKeyboard } from "@/views/Keyboard/style";
 import { Catalog, BaseProduct, Brand as BrandT } from "@/components/Catalog";
 import productsJson from "./_keyboard.json";
 
-type KeyboardBrand = "logig" | "logio" | "logiocb";
+type KeyboardBrand = "logig" | "logio" | "logiocb" | "he";
 
 const brands = [
   { key: "logig", label: "Logitech G"},
   { key: "logio", label: "Logi Văn Phòng"},
   { key: "logiocb", label: "Logi Văn Phòng Combo"},
+  { key: "he", label: "HE Rapid Trigger"},
 ] as const satisfies ReadonlyArray<BrandT<KeyboardBrand>>;
 
 const products = (productsJson as any[])
