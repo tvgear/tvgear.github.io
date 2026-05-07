670a1685f7a8eb249934d5cc272f234e0861a651
votienthuan97 <votienthuan97@gmail.com>
2026-05-07 23:35:31 +0700

    Update Product

--- src/views/Keyboard/index.tsx
+++ src/views/Keyboard/index.tsx
@@ -9,7 +9,6 @@ const brands = [
   { key: "logig", label: "Logitech Gaming"},
   { key: "logio", label: "Logi Văn Phòng"},
   { key: "logiocb", label: "Combo Logi"},
-  { key: "liquidation", label: "Hàng Thanh Lý"},
 ] as const satisfies ReadonlyArray<BrandT<KeyboardBrand>>;
 
 const products = (productsJson as any[])
